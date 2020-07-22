b9df6bea9e781269959250615ba49b3f49eca225
Guy Amsalem <you@example.com>
2020-07-22 21:48:59 +0300

    first

---
+++ src/views/home-page.vue
@@ -0,0 +1,53 @@
+<template>
+  <section class="home-page">
+    <div class="header">
+      <img class="logo" src="../assets/img/logo-clear.png" alt="">
+      <div class="bio">
+      <h2 class="title">לירז שמריז</h2>
+      <p class="sub-title">עו"ס מטפלת ומדריכה למיניות נשית וזוגית</p>
+      </div>
+    </div>
+    <div class="contact">
+      <div class="contact-card">
+        <a href="tel:+972523974614">
+          <i class="fas fa-phone icon"></i>
+          <p>שיחה</p>
+        </a>
+      </div>
+      <div class="contact-card">
+        <a href="https://api.whatsapp.com/send?phone=972523974614" target="_blank">
+          <i class="fab fa-whatsapp icon"></i>
+          <p>וואטסאפ</p>
+        </a>
+      </div>
+      <div class="contact-card">
+        <a href="https://www.facebook.com/liraz.amsalem" target="_blank">
+          <i class="fab fa-facebook-f icon"></i>
+          <p>פייסבוק</p>
+        </a>
+      </div>
+      <div class="contact-card">
+        <a href="https://www.messenger.com/t/liraz.amsalem" target="_blank">
+          <i class="fab fa-facebook-messenger icon"></i>
+          <p>מסנג'ר</p>
+        </a>
+      </div>
+      <div class="contact-card">
+        <a href="mailto:lirazsham@gmail.com">
+          <i class="far fa-envelope icon"></i>
+          <p>אימייל</p>
+        </a>
+      </div>
+      <div class="contact-card">
+        <a href="http://liraz-shamriz.co.il/%d7%a2%d7%9e%d7%95%d7%93-%d7%94%d7%91%d7%99%d7%aa/" target="_blank">
+        <i class="fas fa-home icon"></i>
+        <p>אתר הבית</p>
+        </a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
